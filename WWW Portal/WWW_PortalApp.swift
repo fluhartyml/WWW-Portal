4ca419eb2d68d2053046dd73c7700f9a4e16550a
Michael Fluharty <michael.fluharty@mac.com>
2025-10-30 09:35:16 -0500

    Initial Commit

---
+++ WWW Portal/WWW_PortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWW_PortalApp.swift
+//  WWW Portal
+//
+//  Created by Michael Fluharty on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WWW_PortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WWW PortalTests/WWW_PortalTests.swift
@@ -0,0 +1,17 @@
+//
+//  WWW_PortalTests.swift
+//  WWW PortalTests
+//
+//  Created by Michael Fluharty on 10/30/25.
+//
+
+import Testing
+@testable import WWW_Portal
+
+struct WWW_PortalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
